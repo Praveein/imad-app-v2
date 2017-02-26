7d09b89cd324443c44c4e0a3ccec79d8db797dd6
Praveein <praveein1999@gmail.com>
2017-02-26 23:04:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ button.onclick=function(){
      //make request to the counter endpoint
      
      //capture the endpoint and store it in a variable
-     var request=XMLHttpRequest();
+     var request=new XMLHttpRequest();
      request.onreadystatechange=function(){
          if(request.readystate===XMLHttpRequest.DONE){
              if(request.status===200){
