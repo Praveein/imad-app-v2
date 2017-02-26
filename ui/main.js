2c4052bb7baac0d9b6de4ed1e9bcb1fbf2da2f4a
Praveein <praveein1999@gmail.com>
2017-02-26 22:58:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,23 @@
 var button=document.getElementById("counter");
-var counter=0;
+
 button.onclick=function(){
      //make request to the counter endpoint
      
      //capture the endpoint and store it in a variable
+     var request=XMLHttpRequest();
+     request.onreadystatechange=function(){
+         if(request.readystate===XMLHttpRequest.DONE){
+             if(request.status===200){
+             var counter= request.responseText;
+             var span=document.getElementById('count');
+             span.innerHTML=counter.toString();
+             }
+     }
+     };
      
-     //Render the variable in the correct span
-     counter=counter+1;
-     var span=document.getElementById('count');
-     span.innerHTML=counter.toString();
+     //Render the variable in 
+     request.open("GET",'http://praveein.imad.hasura-app.io/counter',true);
+     request.send(null);
+     
+     
 };
